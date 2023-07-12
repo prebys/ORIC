703baa068920f081116647c98dab584d49ba3011
Eric Prebys <eric.prebys@gmail.com>
2023-07-12 10:41:55 -0700

    updated batch files in JPBFIELD

--- 5.Source Codes/JBPFIELD/CFM.bat
+++ 5.Source Codes/JBPFIELD/CFM.bat
@@ -1,1 +1,4 @@
-FL /Fs /W2  jbfldman.for
+REM Updated to work with Intel compiler
+REM 20230712 EjP
+REM FL /Fs /W2  jbpfield.for
+ifort -c jbpfield.for
--- 5.Source Codes/JBPFIELD/CMLL.bat
+++ 5.Source Codes/JBPFIELD/CMLL.bat
@@ -1,1 +1,4 @@
-FL /W2 /Fs  jbpfield.for ..\jbaccel\acclib.obj
+REM Updated to work with Intel compiler
+REM 20230712 EjP
+REM FL /W2 /Fs  jbpfield.for ..\jbaccel\acclib.obj
+ifort jbpfield.for ..\jbaccel\acclib.obj
